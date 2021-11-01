b0d857cdb8ced8c9aeb0a91f4ae5b0e34086a8ae
ixek <ix3kpl@gmail.com>
2021-11-01 22:48:13 +0100

    Add files via upload

--- src/events/autoreactions.js
+++ src/events/autoreactions.js
@@ -6,6 +6,9 @@
 module.exports = {
     name: 'messageCreate',
     execute(message, client) {
+        //vars
+        const cnl = require('../../data/channels.json');
+
         let upvote = '779116005284642857';
         let downvote = '779116034794979359';
         let plusjeden = '837260568351997993';
@@ -14,7 +17,7 @@ module.exports = {
         let kekw = '837262841375490087';
 
         //memy
-        if(message.channel.id === '761695458598453278') {
+        if(message.channel.id === cnl.memychannel) {
             if(message.attachments.size > 0 || message.content.includes('https://') || message.content.includes('http://') || message.content.includes('#mem')) {
                 message.react(upvote)
                 message.react(downvote)
@@ -22,11 +25,18 @@ module.exports = {
                 message.react(kekw)
             };
         //pokaz swoje jedzonko
-        } else if(message.channel.id === '779103913680699423') {
+        } else if(message.channel.id === cnl.pokazswojejedzonkochannel) {
             if (message.attachments.size > 0 || message.content.includes('https://') || message.cont.includes('https://')){
                 message.react(upvote)
                 message.react(downvote)
             };
-        } 
+        } else if(message.channel.id === cnl.memynsfwchannel) {
+            if(message.attachments.size > 0 || message.content.includes('https://') || message.content.includes('http://') || message.content.includes('#mem')) {
+                message.react(upvote)
+                message.react(downvote)
+                message.react(cringe)
+                message.react(kekw)
+            };
+        };
     }
 }
