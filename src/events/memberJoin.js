7c655cc645d4fea66828cc3e85550564624e6f9f
ixek <ix3kpl@gmail.com>
2022-01-11 00:28:59 +0100

    Add files via upload

--- src/events/memberJoin.js
+++ src/events/memberJoin.js
@@ -4,18 +4,21 @@
 const cnl = require('../../data/channels.json');
 const blacklist = require('../../data/blacklist.json');
 const {MessageEmbed, MessageButton} = require('discord.js');
+const fs = require('fs');
 module.exports = {
     name: 'guildMemberAdd',
     execute(member) {
         //code
         if(blacklist.hellup.includes(member.user.id)) {
+            member.send(`:x: Twoje konto jest na **blackliście**, w związku z tym, zostało automatycznie zbanowane. Jeżeli uważasz, że to pomyłka, zapraszamy do kontaktu pod: \`support@hellup.pl\`!`).catch(err => {
+                if(err) console.log('Blacklist message wasnt send!');
+            })
             member.ban({reason:'Blacklisted user - Autoban'}).catch(e => {
                 if(e) console.log(e);
             });
-            var logs = member.guild.id.channels.cache.get(cnl.logschannel);
+            var logs = member.guild.channels.cache.get(cnl.logschannel);
             const db = require('../../data/maindata.json');
             var casenumber = Number(db.casenumber);
-            console.log(logs)
 
             //logembed
             const logembed = new MessageEmbed()
